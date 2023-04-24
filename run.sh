03152a5fd4ec5cbdc6f6736ef4012f591b270a24
Alexis Ross <alexisro@dgx2.csail.mit.edu>
2023-04-24 10:31:21 -0400

    changes?

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ for LOG in 1 5 20
 do
 	for PRIOR in 0.2 0.5
 	do
-		python main.py --exp_dir results/0419/batch_prior=${PRIOR}-log_ratio=${LOG} --log_log_alpha_ratio ${LOG} --prior_prob ${PRIOR} --wandb_project lm-informants_0419 --num_steps 150 --num_runs 1
+		python main.py --exp_dir results/0419/batch_prior=${PRIOR}-log_ratio=${LOG} --log_log_alpha_ratio ${LOG} --prior_prob ${PRIOR} --wandb_project lm-informants_0419 --num_steps 150 --num_runs 3
 	done
 done
