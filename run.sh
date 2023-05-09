d723d62aa5892f0cdef519a7e7a12541b0d0a6f5
cbreiss <canaanbreiss1@gmail.com>
2023-05-08 22:30:24 -0400

    updates

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
-for LOG in 2
+for LOG in 1
 do
 	for PRIOR in 0.1
 	do
-		python main.py --exp_dir results/0504/warm/batch_prior=${PRIOR}-log_ratio=${LOG} --log_log_alpha_ratio ${LOG} --prior_prob ${PRIOR} --wandb_project lm-informants_maybefinal_three --num_steps 150 --num_runs 10 --tags "maybefinal_three" --feature_type "atr_harmony" --eval_humans --warm_start
-		python main.py --exp_dir results/0504/batch_prior=${PRIOR}-log_ratio=${LOG} --log_log_alpha_ratio ${LOG} --prior_prob ${PRIOR} --wandb_project lm-informants_maybefinal_three --num_steps 150 --num_runs 10 --tags "maybefinal_three" --feature_type "atr_harmony" --eval_humans
+		python main.py --exp_dir results/0508/warm/batch_prior=${PRIOR}-log_ratio=${LOG} --log_log_alpha_ratio ${LOG} --prior_prob ${PRIOR} --wandb_project lm-informants_english --num_steps 150 --num_runs 3 --tags "english" --feature_type "english" --eval_humans --warm_start
 	done
 done
