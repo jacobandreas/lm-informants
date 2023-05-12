92a0a60e7ec20709ca8b156dd497d0a80423fcde
cbreiss <canaanbreiss1@gmail.com>
2023-05-12 12:15:31 -0400

    update canaan runs

--- run_canaan.sh
+++ run_canaan.sh
@@ -1,9 +1,9 @@
-EXP_DIR=results/0510
+EXP_DIR=results/0511
 
-for LOG in .5 1 2 4 8
+for LOG in 8
 do
-	for PRIOR in .1 .2 .35
+	for PRIOR in .35
 	do
-		python main.py --exp_dir ${EXP_DIR}/warm/batch_prior=${PRIOR}-log_ratio=${LOG} --log_log_alpha_ratio ${LOG} --prior_prob ${PRIOR} --wandb_project maybefinal_four --num_steps 150 --num_runs 10 --tags "maybefinal_four" --feature_type "atr_harmony" --eval_humans --strategies unif train entropy entropy_pred
+		python main.py --exp_dir ${EXP_DIR}/warm/batch_prior=${PRIOR}-log_ratio=${LOG} --log_log_alpha_ratio ${LOG} --prior_prob ${PRIOR} --wandb_project maybefinal_four --num_steps 150 --num_runs 10 --tags "maybefinal_four" --feature_type "atr" --eval_humans --strategies eig_train_model 
 	done
 done
