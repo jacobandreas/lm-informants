89f870bc9f0167f7da28a7a5809b2a3bb30ea6fa
cbreiss <canaanbreiss1@gmail.com>
2023-05-15 10:34:41 -0400

    canaan continue atr

--- run_canaan.sh
+++ run_canaan.sh
@@ -2,7 +2,7 @@ EXP_DIR=results/0510
 
 for LOG in .5 
 do
-	for PRIOR in .2 .35
+	for PRIOR in .35
 	do
 		python main.py --exp_dir ${EXP_DIR}/warm/batch_prior=${PRIOR}-log_ratio=${LOG} --log_log_alpha_ratio ${LOG} --prior_prob ${PRIOR} --wandb_project maybefinal_four --num_steps 150 --num_runs 10 --tags "maybefinal_four" --feature_type "atr_harmony" --eval_humans --strategies kl kl_train_model kl_train_history kl_train_mixed eig eig_train_model eig_train_history eig_train_mixed
 	done
