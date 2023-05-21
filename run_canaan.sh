5693191e5ad04cd4c3e4423a913003c662c13059
cbreiss <canaanbreiss1@gmail.com>
2023-05-22 03:58:39 +0900

    kick off runs for canaan

--- run_canaan.sh
+++ run_canaan.sh
@@ -1,8 +1,8 @@
 EXP_DIR=results/0510
 
-for LOG in .1 .25 
+for LOG in ..5 1 2 4 8
 do
-	for PRIOR in .05 .1 .2 .35
+	for PRIOR in .05 
 	do
 		python main.py --exp_dir ${EXP_DIR}/warm/batch_prior=${PRIOR}-log_ratio=${LOG} --log_log_alpha_ratio ${LOG} --prior_prob ${PRIOR} --wandb_project maybefinal_four --num_steps 150 --num_runs 10 --tags "maybefinal_four" --feature_type "atr_harmony" --eval_humans --strategies entropy entropy_pred train unif
 	
