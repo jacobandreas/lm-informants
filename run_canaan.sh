7c03117c7c65b51007f441bcfaaca9ac154865a4
cbreiss <canaanbreiss1@gmail.com>
2023-05-30 19:36:16 -0400

    ad

--- run_canaan.sh
+++ run_canaan.sh
@@ -4,7 +4,7 @@ for LOG in 8
 do
 	for PRIOR in .1 .2 .35 
 	do
-		python main.py --exp_dir ${EXP_DIR}/warm/batch_prior=${PRIOR}-log_ratio=${LOG} --log_log_alpha_ratio ${LOG} --prior_prob ${PRIOR} --wandb_project maybefinal_four --num_steps 150 --num_runs 10 --tags "maybefinal_four" --feature_type "atr_harmony" --eval_humans --strategies entropy entropy_pred train unif
+		python main.py --exp_dir ${EXP_DIR}/warm/batch_prior=${PRIOR}-log_ratio=${LOG} --log_log_alpha_ratio ${LOG} --prior_prob ${PRIOR} --wandb_project maybefinal_four --num_steps 150 --num_runs 10 --tags "maybefinal_four" --feature_type "atr_harmony" --eval_humans --strategies kl kl_train_model kl_train_history kl_train_mixed eig eig_train_model eig_train_history eig_train_mixed
 	
 	done
 done
