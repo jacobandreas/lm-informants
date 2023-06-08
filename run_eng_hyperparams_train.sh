e99db00259eee2b1491f5e2a3600745d5b23588f
Alexis Ross <alexisro@dgx2.csail.mit.edu>
2023-06-08 14:17:38 -0400

    update hyperparam calcs

--- run_eng_hyperparams_train.sh
+++ run_eng_hyperparams_train.sh
@@ -6,24 +6,24 @@ NUM_STEPS=200
 NUM_CANDS=50
 
 # prior p_all_off of 0.5
-PRIOR=0.00138533389897108
-for LLA in 0.818807677680882 1.32677908055553 1.65032042172503 2.13636980699140 
+PRIOR=0.000385435812071119
+for LLA in 0.818508339363156 1.32655384099565 1.65014245712181 2.13625332091839 
 do
 	python main.py --exp_dir ${EXP_DIR}/warm/batch_prior=${PRIOR}-log_ratio=${LLA} --log_log_alpha_ratio ${LLA} --prior_prob ${PRIOR} --wandb_project $WANDB_PROJECT --num_steps $NUM_STEPS --num_runs 1 --feature_type "english" --eval_humans --strategies train --feature_type english --num_candidates $NUM_CANDS --tags $TAG 
 	#--verbose 
 done
 
 # prior p_all_off of 0.3
-PRIOR=0.00240504883318384
-for LLA in 1.72665652700838 2.09477871455906 2.35358707233326 2.76987047901232
+PRIOR=0.000669393655992854
+for LLA in 1.72623647509002 2.09445314096582 2.35332338102535 2.76969036320956 
 do
 	python main.py --exp_dir ${EXP_DIR}/warm/batch_prior=${PRIOR}-log_ratio=${LLA} --log_log_alpha_ratio ${LLA} --prior_prob ${PRIOR} --wandb_project $WANDB_PROJECT --num_steps $NUM_STEPS --num_runs 1 --feature_type "english" --eval_humans --strategies train --feature_type english --num_candidates $NUM_CANDS --tags $TAG 
 	#--verbose 
 done
 
 # prior p_all_off of 0.1
-PRIOR=0.00459458264847305
-for LLA in 3.15104773856974 3.42965456729417 3.63928993623763 3.99571598252335 
+PRIOR=0.00127981720494661
+for LLA in 3.15040457368557 3.42914027054695 3.63886266492049 3.99541135890715
 do
 	python main.py --exp_dir ${EXP_DIR}/warm/batch_prior=${PRIOR}-log_ratio=${LLA} --log_log_alpha_ratio ${LLA} --prior_prob ${PRIOR} --wandb_project $WANDB_PROJECT --num_steps $NUM_STEPS --num_runs 1 --feature_type "english" --eval_humans --strategies train --feature_type english --num_candidates $NUM_CANDS --tags $TAG 
 	#--verbose 
