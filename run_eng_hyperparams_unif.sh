038fd49ef5d7ea572e0fca7a5e241287b835793f
Alexis Ross <alexisro@dgx2.csail.mit.edu>
2023-08-01 17:46:22 -0400

    update run file

--- run_eng_hyperparams_unif.sh
+++ run_eng_hyperparams_unif.sh
@@ -4,26 +4,29 @@ TAG=0801_hyperparam_sweep,fixed_eval_sets
 NUM_STEPS=200
 NUM_CANDS=50
 
-# prior p_all_off of 0.5
-PRIOR=0.001385
-# commenting out bc already ran, restarted
-for LLA in -0.582269524436029 0.100113 0.522736 0.87727 1.075687 1.333741 
+for MAX_UPDATES in None 1
 do
-	python main.py --exp_dir ${EXP_DIR}/warm/batch_prior=${PRIOR}-log_ratio=${LLA} --log_log_alpha_ratio ${LLA} --prior_prob ${PRIOR} --wandb_project $WANDB_PROJECT --num_steps $NUM_STEPS --num_runs 1 --feature_type "english" --eval_humans --strategies unif --feature_type english --num_candidates $NUM_CANDS --tags $TAG --max_updates_observe 1
-	#--verbose 
-done
+	# prior p_all_off of 0.5
+	PRIOR=0.001385
+	# commenting out bc already ran, restarted
+	for LLA in -0.582269524436029 0.100113 0.522736 0.87727 1.075687 1.333741 
+	do
+		python main.py --exp_dir ${EXP_DIR}/warm/batch_prior=${PRIOR}-log_ratio=${LLA} --log_log_alpha_ratio ${LLA} --prior_prob ${PRIOR} --wandb_project $WANDB_PROJECT --num_steps $NUM_STEPS --num_runs 1 --feature_type "english" --eval_humans --strategies unif --feature_type english --num_candidates $NUM_CANDS --tags $TAG --max_updates_observe ${MAX_UPDATES} 
+		#--verbose 
+	done
 
-# prior p_all_off of 0.3
-PRIOR=0.002405
-for LLA in -0.03161 0.77243 1.113339 1.500334 
-do
-	python main.py --exp_dir ${EXP_DIR}/warm/batch_prior=${PRIOR}-log_ratio=${LLA} --log_log_alpha_ratio ${LLA} --prior_prob ${PRIOR} --wandb_project $WANDB_PROJECT --num_steps $NUM_STEPS --num_runs 1 --feature_type "english" --eval_humans --strategies unif --feature_type english --num_candidates $NUM_CANDS --tags $TAG --max_updates_observe 1
-	#--verbose 
-done
+	# prior p_all_off of 0.3
+	PRIOR=0.002405
+	for LLA in -0.03161 0.77243 1.113339 1.500334 
+	do
+		python main.py --exp_dir ${EXP_DIR}/warm/batch_prior=${PRIOR}-log_ratio=${LLA} --log_log_alpha_ratio ${LLA} --prior_prob ${PRIOR} --wandb_project $WANDB_PROJECT --num_steps $NUM_STEPS --num_runs 1 --feature_type "english" --eval_humans --strategies unif --feature_type english --num_candidates $NUM_CANDS --tags $TAG --max_updates_observe ${MAX_UPDATES} 
+		#--verbose 
+	done
 
-# prior p_all_off of 0.1
-PRIOR=0.004595
-for LLA in 0.970219 1.939392 
-do
-	python main.py --exp_dir ${EXP_DIR}/warm/batch_prior=${PRIOR}-log_ratio=${LLA} --log_log_alpha_ratio ${LLA} --prior_prob ${PRIOR} --wandb_project $WANDB_PROJECT --num_steps $NUM_STEPS --num_runs 1 --feature_type "english" --eval_humans --strategies unif --feature_type english --num_candidates $NUM_CANDS --tags $TAG --max_updates_observe 1
+	# prior p_all_off of 0.1
+	PRIOR=0.004595
+	for LLA in 0.970219 1.939392 
+	do
+		python main.py --exp_dir ${EXP_DIR}/warm/batch_prior=${PRIOR}-log_ratio=${LLA} --log_log_alpha_ratio ${LLA} --prior_prob ${PRIOR} --wandb_project $WANDB_PROJECT --num_steps $NUM_STEPS --num_runs 1 --feature_type "english" --eval_humans --strategies unif --feature_type english --num_candidates $NUM_CANDS --tags $TAG --max_updates_observe ${MAX_UPDATES} 
+	done
 done
