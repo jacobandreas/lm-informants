022392f07dced47188e0d1a38ca316e1538d2444
Alexis Ross <alexisro@dgx2.csail.mit.edu>
2023-08-01 14:54:31 -0400

    updated scripts for running experiments

--- run_eng_hyperparams_kl.sh
+++ run_eng_hyperparams_kl.sh
@@ -1,13 +1,12 @@
-EXP_DIR=results/0621_english
-WANDB_PROJECT=0704_english
-#TAG=0606_hyperparams_selected
-TAG=0713_hyperparams
+EXP_DIR=results/0801_english
+WANDB_PROJECT=0801_english
+TAG=0801_hyperparam_sweep,fixed_eval_sets
 NUM_STEPS=200
 NUM_CANDS=50
 
 # prior p_all_off of 0.5
 PRIOR=0.001385
-# commenting out bc already ran -2.12616, restarted
+# commenting out bc already ran, restarted
 for LLA in -0.582269524436029 0.100113 0.522736 0.87727 1.075687 1.333741 
 do
 	python main.py --exp_dir ${EXP_DIR}/warm/batch_prior=${PRIOR}-log_ratio=${LLA} --log_log_alpha_ratio ${LLA} --prior_prob ${PRIOR} --wandb_project $WANDB_PROJECT --num_steps $NUM_STEPS --num_runs 1 --feature_type "english" --eval_humans --strategies kl_train_model --feature_type english --num_candidates $NUM_CANDS --tags $TAG --max_updates_observe 1 --max_updates_propose 1 
