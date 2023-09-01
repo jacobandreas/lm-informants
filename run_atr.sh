e570160f20af3a672b3a31ef0962a94bb46e3e90
Alexis Ross <alexisro@dgx2.csail.mit.edu>
2023-09-01 15:26:39 -0400

    add atr script

---
+++ run_atr.sh
@@ -0,0 +1,20 @@
+WANDB_PROJECT=0901_atr
+
+for LOG in 0.1 0.25 0.5 1 2 4 8 
+do
+	for PRIOR in 0.05 0.1 0.2 0.35
+	do
+		for MAX_UPDATES in None 1
+		do
+		python main.py --exp_dir results/0610_test/warm/batch_prior=${PRIOR}-log_ratio=${LOG} \
+		--log_log_alpha_ratio ${LOG} \
+		--prior_prob ${PRIOR} \
+		--wandb_project $WANDB_PROJECT \
+		--num_steps 150 --num_runs 3 --tags "0901_test_atr" \
+		--feature_type "atr_harmony" --eval_humans \
+		--max_updates_observe $MAX_UPDATES \
+		--max_updates_propose $MAX_UPDATES 
+#		--strategies eig 
+		done
+	done
+done
