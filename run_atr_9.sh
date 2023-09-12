6a58d3cf14bd274ef8bec74724ef0e7f376608ee
EC2 Default User <ec2-user@ip-172-31-17-204.us-east-2.compute.internal>
2023-09-12 14:59:51 +0000

    add files

---
+++ run_atr_9.sh
@@ -0,0 +1,42 @@
+WANDB_PROJECT=0901_atr
+RUN=9
+
+for LOG in 1 
+do
+	for PRIOR in 0.2 0.35
+	do
+		for MAX_UPDATES in None 1
+		do
+		python main.py --exp_dir results/0610_test/warm/batch_prior=${PRIOR}-log_ratio=${LOG} \
+		--log_log_alpha_ratio ${LOG} \
+		--prior_prob ${PRIOR} \
+		--wandb_project $WANDB_PROJECT \
+		--num_steps 150 --num_runs 1 --tags "0906_aws" \
+		--feature_type "atr_harmony" --eval_humans \
+		--max_updates_observe $MAX_UPDATES \
+		--max_updates_propose $MAX_UPDATES \
+	        --start_run ${RUN}	
+#		--strategies eig 
+		done
+	done
+done
+
+for LOG in 2 4 8 
+do
+	for PRIOR in 0.05 0.1 0.2 0.35
+	do
+		for MAX_UPDATES in None 1
+		do
+		python main.py --exp_dir results/0610_test/warm/batch_prior=${PRIOR}-log_ratio=${LOG} \
+		--log_log_alpha_ratio ${LOG} \
+		--prior_prob ${PRIOR} \
+		--wandb_project $WANDB_PROJECT \
+		--num_steps 150 --num_runs 1 --tags "0906_aws" \
+		--feature_type "atr_harmony" --eval_humans \
+		--max_updates_observe $MAX_UPDATES \
+		--max_updates_propose $MAX_UPDATES \
+	        --start_run ${RUN}	
+#		--strategies eig 
+		done
+	done
+done
