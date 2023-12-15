1a9fd728e0098c4100ab78255041afa6966e1166
EC2 Default User <ec2-user@ip-172-31-10-181.us-east-2.compute.internal>
2023-12-15 00:03:12 +0000

    add canaan files

---
+++ run_1214_canaan_1.sh
@@ -0,0 +1,27 @@
+EXP_DIR=results/1214_english_zipfian
+WANDB_PROJECT=1214_english_zipfian
+TAG=canaan
+NUM_STEPS=200
+NUM_CANDS=100
+
+# prior p_all_off of 0.5
+PRIOR=0.00227035682449808
+LLA=1.47488617047509
+#5.25792364149749
+python main.py --exp_dir ${EXP_DIR}/prior=${PRIOR}-log_ratio=${LLA}-seed=${SEED} \
+	--log_log_alpha_ratio ${LLA} \
+	--prior_prob ${PRIOR} \
+	--wandb_project $WANDB_PROJECT \
+	--num_steps $NUM_STEPS \
+	--num_runs 1 \
+	--start_run 2 \
+	--feature_type "english" \
+	--eval_humans \
+	--strategies eig_train_mixed \
+	--feature_type english \
+	--num_candidates $NUM_CANDS \
+	--tags $TAG \
+	--max_updates_observe 1 \
+	--max_updates_propose 1 \
+	--use_zipfian \
+	--train_expect_type poisson_samples
--- run_1214_canaan_2.sh
+++ run_1214_canaan_2.sh
@@ -1,7 +1,7 @@
 EXP_DIR=results/1214_english_zipfian
 WANDB_PROJECT=1214_english_zipfian
 TAG=canaan
-NUM_STEPS=250
+NUM_STEPS=200
 NUM_CANDS=100
 
 # prior p_all_off of 0.5
@@ -13,11 +13,11 @@ python main.py --exp_dir ${EXP_DIR}/prior=${PRIOR}-log_ratio=${LLA}-seed=${SEED}
 	--prior_prob ${PRIOR} \
 	--wandb_project $WANDB_PROJECT \
 	--num_steps $NUM_STEPS \
-	--num_runs 2 \
-	--start_run 0 \
+	--num_runs 1 \
+	--start_run 3 \
 	--feature_type "english" \
 	--eval_humans \
-	--strategies eig eig_train_mixed unif \
+	--strategies eig_train_mixed \
 	--feature_type english \
 	--num_candidates $NUM_CANDS \
 	--tags $TAG \
