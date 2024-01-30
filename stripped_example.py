e9ba94fd337251f100949b15797e5a0197565f17
amanirmk <amainakilaas@hmc.edu>
2024-01-30 16:14:22 -0500

    playing w stripped example - might not keep

--- stripped_example.py
+++ stripped_example.py
@@ -2,8 +2,6 @@ import jax
 import jax.numpy as jnp
 from BayesianGLM import BayesianScorer
 
-n_features = 2
-
 alpha_prior_mu=5.0
 alpha_prior_sigma=1.0
 beta_prior_mu=-10.0
@@ -12,14 +10,16 @@ step_size=0.01
 n_updates=2000
 rng_key=jax.random.PRNGKey(0)
 
-init_params = {
-    "alpha_posterior_mu": jnp.full(1, alpha_prior_mu),
-    "alpha_posterior_sigma": jnp.full(1, alpha_prior_sigma),
-    "beta_posterior_mu": jnp.full(n_features, beta_prior_mu),
-    "beta_posterior_sigma": jnp.full(n_features, beta_prior_sigma)
-}
+def initialize_params(n_features):
+  return {
+      "alpha_posterior_mu": jnp.full(1, alpha_prior_mu),
+      "alpha_posterior_sigma": jnp.full(1, alpha_prior_sigma),
+      "beta_posterior_mu": jnp.full(n_features, beta_prior_mu),
+      "beta_posterior_sigma": jnp.full(n_features, beta_prior_sigma)
+  }
 
 def expected_metric(featurized_seq):
+    init_params = initialize_params(len(featurized_seq))
     logit = sum(featurized_seq)*beta_prior_mu + alpha_prior_mu
     prob_pos = jax.nn.sigmoid(logit)
 
@@ -46,13 +46,27 @@ def expected_metric(featurized_seq):
 
     eig = prob_pos * pos_ig + (1 - prob_pos) * neg_ig
     ekl = prob_pos * pos_kl + (1 - prob_pos) * neg_kl
-    return eig, ekl
+
+    # recreate eig
+    old_entropy = BayesianScorer.entropy(init_params)
+    pos_entropy = BayesianScorer.entropy(pos_params)
+    neg_entropy = BayesianScorer.entropy(neg_params)
+    expected_new_entropy = prob_pos * pos_entropy + (1 - prob_pos) * neg_entropy
+    eig_derived = 2*ekl + expected_new_entropy - old_entropy
+    return eig, eig_derived, ekl
 
 
 # TEST HERE
 featurized_seq = [0, 1] # binary
-
-assert len(featurized_seq) == n_features
-eig, ekl = expected_metric(featurized_seq)
+eig, eig_derived, ekl = expected_metric(featurized_seq)
 print("EIG:", eig)
+print("EIG':", eig_derived)
 print("EKL:", ekl)
+
+# for n_features in range(10):
+#     for k in range(n_features):
+#       f = [0.0] * (n_features-k) + [1.0] * k
+#       print(f)
+#       eig, eig_derived, _ = expected_metric(featurized_seq)
+#       assert eig == eig_derived, f"{k}: {eig}, {eig_derived}"
+#     print(f"PASSED N={n_features}")
